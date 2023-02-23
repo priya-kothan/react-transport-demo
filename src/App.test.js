6af99612e7ae8a757ab3a20ac0152489ea483fdf
Priya R6 <priya.r6@ibm.com>
2023-02-23 13:04:11 +0530

    basic test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Welcome as a text', () => {
+  // Arrange
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Act
+  // ... nothing
+
+  // Assert
+  const helloWorldElement = screen.getByText('Welcome');
+  expect(helloWorldElement).toBeInTheDocument();
 });
